37e7a01a1180499cc7f7d135d9adea5d5c798410
Salvador Guzman <guzmansalv@gmail.com>
2018-05-27 18:59:21 -0700

    working get method

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include "../crow/amalgamate/crow_all.h"
+#include "http_get.hxx"
 
 using std::cout;
 using std::cerr;
 using std::endl;
 
+constexpr const static unsigned short cores{8};
+
 int main() noexcept {
   cout << "Hello world" << endl;
+
+  cout << Curl::get("https://raw.githubusercontent.com/sguzman/BrotliEncoderService/master/build.sc") << endl;
 
   return EXIT_SUCCESS;
 }
